34ded5a36c45746762e3b59dedb66c8ff3410a5f
Eduardo Baranowski <35973050+Eduardo-Baranowski@users.noreply.github.com>
2018-07-29 11:26:04 -0300

    commit edu

--- main.js
+++ main.js
@@ -1,33 +1,73 @@
 var noticias = [];
+var now = new Date()
 
-function atualizarLista(noticia) {
-    var lista = document.getElementById('noticias-recentes-list');
-    var li = document.createElement('li');
-    li.setAttribute('id', 'noticia-' + noticia.id);
-    li.setAttribute('class', 'noticia');
-    li.innerHTML = '<p class="titulo" onclick="mostrarNoticia(' + noticia.id + ')">'
-        + noticia.titulo
-        + '</p>'
-        + '<p class="conteudo">'
-        + noticia.conteudo
-        + '<br>'
+if(now.getMonth()<9){
+  var mes = '0'+(now.getMonth()+1);
+} else {
+  var mes = (now.getMonth()+1);
+}
+
+var datetime = now.getFullYear()+"-"
+  + mes+"-"
+  + now.getDate()+ "T"
+  + now.getHours() + ":"
+  + now.getMinutes();
+console.log(datetime);
+
+function atualizarLista(noticia){
+
+  var data = noticia.dataehora;
+  console.log(data);
+
+  if(data != 0){
+    if(data<= datetime){
+      var lista = document.getElementById('noticias-recentes-list');
+      var li = document.createElement('li');
+      li.setAttribute('id', 'noticia-' + noticia.id);
+      li.setAttribute('class', 'noticia');
+      li.innerHTML = '<p class="titulo" onclick="mostrarNoticia('+ noticia.id +')">'
+        +noticia.titulo +'<br>'
+        +noticia.nome + '<br>'
+        +noticia.dataehora + '<br>'
+        +'</p>'
+        +'<p class="Conteudo">'
+        +noticia.conteudo
+        +'<br>'
         + '<span>------------------</span>'
         + '<br>'
         + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>';
         + '</p>';
-    lista.appendChild(li);
+        lista.appendChild(li);
+    }
+  }
 }
 
+
+
 function salvar(form) {
+    var nome = document.getElementById('frm-autor').value;
+    var email = document.getElementById('frm-email').value;
+    var dataehora = document.getElementById('frm-dataehora').value;
     var titulo = document.getElementById('frm-titulo').value;
     var conteudo = document.getElementById('frm-conteudo').value;
     var noticia = {
         id: noticias.length,
+        nome: nome,
+        email: email,
+        dataehora: dataehora,
         titulo: titulo,
         conteudo: conteudo
     };
+
     noticias.push(noticia);
-    atualizarLista(noticia);
+    noticias.sort((noticia1, noticia2) => noticia2.data-noticia1.data);
+    console.log(noticias)
+    var myList = document.getElementById('noticias-recentes-list');
+    myList.innerHTML = '';
+
+    for(var i = 0; i <noticias.length; i++){
+      atualizarLista(noticias[i]);
+    }
     form.reset();
 }
 
